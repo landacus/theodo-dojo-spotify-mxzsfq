287ff5aa9ad6931c1ae22ad6c9bf0a09fdacba7e
landacus <34149197+landacus@users.noreply.github.com>
2023-05-31 19:33:52 +0200

    Import Spotify

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,17 @@
-const apiToken: string = '';
+const apiToken: string =
+  'BQDIJq_AKP9YiqD7oyBAPMukKkIbXrd5MRq7FrUekpNM0PLnRlOZiXYZHNPYKW1H9wSzAz90p_Yb5TUas7tPhxnhbBIHTp9fzObu9nmm3YIdi24RT4s';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: unknown[] };
+
+  return data.items;
 };
